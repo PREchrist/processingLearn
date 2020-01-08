6f643efbf6ecf02e2d3388587589bbd6163533e7
PreChrist2.0 <55775200+PREchrist@users.noreply.github.com>
2020-01-08 13:16:17 +0900

    Create question1.c

---
+++ question1.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+// //Revision of programs
+// //program #1
+// void main(){
+//   char name[60];
+
+//   printf("Write down your user name\n");
+//   scanf("%s", name);
+
+//   for(int i=0; i<10; i++){
+//     printf("Hello dear %s, welcome back in the society %i\n", name, 2020);
+//   }
+// }
+
+//program #2
+// void main(){
+//   int number;
+
+//   printf("Select the number\n");
+//   scanf("%i", &number);
+
+//   //check whether even/odd number
+//   if(number%2==0){
+//     //even number
+//     printf("You selected an even number");
+//   }else{
+//     //odd number
+//     printf("You chose one of odd number");
+//   }
+// }
+
+//program #3
+// int main(){
+//   int number;
+
+//   printf("Write down your number\n");
+//   scanf("%i", &number);
+
+//   //look for whether positive/negative
+//     if(number>=0){
+//       //positive number
+//       if (number==0){
+//         //it might be the same
+//         printf("This number has not position here");
+//       }else{
+//         printf("You got a postive number");
+//       }
+//     }else{
+//       //negative number
+//       printf("You got a negative number");
+//     }
+
+//   return 0;
+// }
+
+// //program #4
+// int main(){
+//   int age;
+
+//   printf("Insert down your age\n");
+//   scanf("%i", &age);
+
+//   //age of eligiblity to vote in japan
+//   if(age>=21){
+//     //The candidate is eligible
+//     printf("Congratulations! you are eligible for casting your vote");
+//   }else{
+//     //the candidate cannot be eligible
+//     printf("Sorry you are still not eligible");
+
+//     return 0;
+//   }
+// }
